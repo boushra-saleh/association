f1fe2e5dcd52e5086b768aa52ae9a6811ed2f199
boushra-saleh <boushraabd585@gmail.com>
2022-06-19 12:32:37 +0300

    first commit

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,5 +1,24 @@
 import * as bootstrap from 'bootstrap'
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})(bootstrap.Popover.prototype.show)
 // Extend Tooltip to add color options
 bootstrap.Tooltip.prototype.show = (original => {
   return function addTooltipColor() {
@@ -27,3 +46,5 @@ bootstrap.Popover.prototype.show = (original => {
 })(bootstrap.Popover.prototype.show)
 
 export { bootstrap }
+
+
